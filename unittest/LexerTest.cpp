c9078c9427943af7427e5ebba501ff49be382c83
drop-stones <shizukuishi.s.aa@m.titech.ac.jp>
2021-12-22 22:51:11 -0800

    Implement Lexer

--- unittest/LexerTest.cpp
+++ unittest/LexerTest.cpp
@@ -4,8 +4,32 @@
 
 namespace {
 
-TEST(TEST, false) {
-  EXPECT_EQ(true, true);
+TEST(LexerTest, tokenizeTest) {
+  Lexer Lex("1 + 2 * 3");
+  Token tok;
+
+  Lex.next(tok);
+  EXPECT_EQ("1", tok.getText());
+  EXPECT_EQ(Token::number, tok.getKind());
+
+  Lex.next(tok);
+  EXPECT_EQ("+", tok.getText());
+  EXPECT_EQ(Token::plus, tok.getKind());
+
+  Lex.next(tok);
+  EXPECT_EQ("2", tok.getText());
+  EXPECT_EQ(Token::number, tok.getKind());
+
+  Lex.next(tok);
+  EXPECT_EQ("*", tok.getText());
+  EXPECT_EQ(Token::star, tok.getKind());
+
+  Lex.next(tok);
+  EXPECT_EQ("3", tok.getText());
+  EXPECT_EQ(Token::number, tok.getKind());
+
+  Lex.next(tok);
+  EXPECT_EQ(Token::eoi, tok.getKind());
 }
 
 } // namespace
